d79c2c93258b2a81d71be751ca5aa5aeb29fb112
Frédéric Suire <fred_suire@yahoo.fr>
2018-02-05 20:39:41 +0100

    int: remove all about https for server

--- server/src/server/http.service.js
+++ server/src/server/http.service.js
@@ -5,10 +5,7 @@ const app = async () => {
   const app = express()
 
   app.get('*', async (req, res) => {
-    const domain = req.headers.host.replace(/\:[0-9]+/, `:${process.env.HTTPS_PORT}`)
-
-    const redirectUrl = `https://${domain}${req.url}`
-    res.redirect(308, redirectUrl)
+    res.send('hello')
   })
 
   return app
