27787b9f7db11836402ca69435ce753b7174bc65
Frédéric Suire <fred_suire@yahoo.fr>
2018-02-10 20:01:17 +0100

    :sparkles: add theme and mixins files for style

---
+++ client/src/style/theme.js
@@ -0,0 +1,20 @@
+export const colors = {
+    primary: '#c562a3',
+    secondary: '#a3c562',
+    tertiary: '#a3c562',
+    neutral: '#888888',
+
+    dark: {
+        primary: '#623151',
+        secondary: '#516231',
+        tertiary: '#315162',
+        neutral: '#444444',
+    },
+
+    light: {
+        primary: '#e2b0d1',
+        secondary: '#d1e2b0',
+        tertiary: '#b0d1e2',
+        neutral: '#c3c3c3',
+    }
+}
